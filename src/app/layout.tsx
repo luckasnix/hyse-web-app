4837443e09c0492d0e163422db285f40b89b426a
Kasnix <luc.kasnix@gmail.com>
2024-12-10 20:18:00 -0300

    feat: initialize the project

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+
+import type { RootLayoutProps } from "./types";
+
+export const metadata: Metadata = {
+  title: "Hyse",
+  description: "Read comics",
+};
+
+const RootLayout = ({ children }: RootLayoutProps) => {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
